d45eb1d91368031a0847653a9e9ce54f3d43bed1
RAZVAN24RR <76512239+RAZVAN24RR@users.noreply.github.com>
2022-01-10 15:13:24 +0200

    sdf

--- src/App.js
+++ src/App.js
@@ -36,46 +36,46 @@ const App = () => {
           <button className="highlight" name="/" onClick={handleClick}>
             &divide;
           </button>
-          <button name="7" onClick={handleClick}>
+          <button className="button-white" name="7" onClick={handleClick}>
             7
           </button>
-          <button name="8" onClick={handleClick}>
+          <button className="button-white" name="8" onClick={handleClick}>
             8
           </button>
-          <button name="9" onClick={handleClick}>
+          <button className="button-white" name="9" onClick={handleClick}>
             9
           </button>
           <button className="highlight" name="*" onClick={handleClick}>
             &times;
           </button>
-          <button name="4" onClick={handleClick}>
+          <button className="button-white" name="4" onClick={handleClick}>
             4
           </button>
-          <button name="5" onClick={handleClick}>
+          <button className="button-white" name="5" onClick={handleClick}>
             5
           </button>
-          <button name="6" onClick={handleClick}>
+          <button className="button-white" name="6" onClick={handleClick}>
             6
           </button>
           <button className="highlight" name="-" onClick={handleClick}>
             &ndash;
           </button>
-          <button name="1" onClick={handleClick}>
+          <button className="button-white" name="1" onClick={handleClick}>
             1
           </button>
-          <button name="2" onClick={handleClick}>
+          <button className="button-white" name="2" onClick={handleClick}>
             2
           </button>
-          <button name="3" onClick={handleClick}>
+          <button className="button-white" name="3" onClick={handleClick}>
             3
           </button>
           <button className="highlight" name="+" onClick={handleClick}>
             +
           </button>
-          <button name="0" onClick={handleClick}>
+          <button className="button-white" name="0" onClick={handleClick}>
             0
           </button>
-          <button name="." onClick={handleClick}>
+          <button className="button-white" name="." onClick={handleClick}>
             .
           </button>
           <button className="highlight" onClick={calculate} id="result">
